108f6775952de08887cbb731edd32b06de929a65
A4RO <102383141+A4RO@users.noreply.github.com>
2024-11-10 01:02:05 +0200

    Update hwsicp-1-2-answers.scm

--- week1/hwsicp-1-2-answers.scm
+++ week1/hwsicp-1-2-answers.scm
@@ -1,6 +1,16 @@
-;1.
+;1. There is an infinite loop which happens because new-if keeps evaluating sqrt-iter even when the guess is good enough. The reason that it keeps evaluating it is that applicative order is used.
 
 ;2.
+#lang simply-scheme
+
+(define (squares numbers)
+  (if (empty? numbers)
+      '()
+      (sentence (square (first numbers))
+          (squares (bf numbers)))))
+
+(define (square x)
+  (* x x))
 
 ;3.
 
